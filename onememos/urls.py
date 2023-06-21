6f24c029d98f6b174495517cfa5ccb8bd5339a27
rheesk22 <rheesk22@dtaas.co.kr>
2023-06-21 12:13:54 +0900

    강의 수강 업데이트
    get, post 구분해서 처리 구현

--- onememos/urls.py
+++ onememos/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
 	path( '' , views.index, name='index' ),
 	path( 'createMemo/' , views.createMemo ),
+    
+	path('writeMemo/', views.writeMemo),
 ]
 
--- onememos/views.py
+++ onememos/views.py
@@ -32,3 +32,13 @@ def createMemo( request ) :
 #	return HttpResponse( "Create Memo = " + memoContent )
 
 	return HttpResponseRedirect( reverse( 'index' ) )
+
+def writeMemo( request ) :
+	if request.method == "GET":
+		return HttpResponse('GET방식 요청=> 폼페이지를 보여주세요')
+#		return render( request, 'my_template.html', {'Method': 'GET 방식'})
+	elif request.method == "POST":
+		return HttpResponse("POST방식 요청=> DB로 넣어주세요")
+#		return render( request, 'my_template.html', {'Method': 'POST 방식'})
+	else :
+		return HttpResponse('writePage요청??')
