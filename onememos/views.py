a911238cf865eb595f418be899ab28aacae62bbb
rheesk22 <rheesk22@dtaas.co.kr>
2023-06-21 11:46:19 +0900

    강의 수강 간 수정
    리스트뷰 생성

--- onememos/views.py
+++ onememos/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import *
 
 # Create your views here.
 
 def index( request ) :
-	#return HttpResponse("Onememos~Hello, World~ ^.~")
-	return render ( request, 'main.html')
+#	return HttpResponse("Onememos~Hello, World~ ^.~")
+
+#	return render ( request, 'main.html')
+	
+	lists = Memo.objects.all()
+	data = { 'lists' : lists }
+	
+	return render ( request, 'main.html', data )
+	
 
 def createMemo( request ) :
 #	memoContent = request.GET.get('memoContent')
@@ -16,8 +24,11 @@ def createMemo( request ) :
 
 #	if memoContent == None :
 #		return HttpResponse( "Create Memo = " )
+
+#	db save	
+#	article = Memo( memo_text = memoContent )
+#	article.save()
 	
-	article = Memo( memo_text = memoContent )
-	article.save()
-	
-	return HttpResponse( "Create Memo = " + memoContent )
+#	return HttpResponse( "Create Memo = " + memoContent )
+
+	return HttpResponseRedirect( reverse( 'index' ) )
